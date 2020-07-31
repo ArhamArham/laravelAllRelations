97a09175f282d5998a530ec2662e378d4dbfdb52
ArhamArham <arhamroshan443@gmail.com>
2020-07-31 16:00:45 +0500

    hasOne-belongsTo

--- app/User.php
+++ app/User.php
@@ -36,4 +36,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function address(){
+        return $this->hasOne(\App\Address::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('user', function () {
+    // factory(\App\User::class,3)->create();
+    // \App\Address::create([
+    //     'user_id'=>1,
+    //     'country'=>'uk'
+    // ]);
+    // \App\Address::create([
+    //     'user_id'=>2,
+    //     'country'=>'pakistan'
+    // ]);
+    // \App\Address::create([
+    //     'user_id'=>3,
+    //     'country'=>'india'
+    // ]);
+    // $users=\App\User::with('address')->get();
+    // $user=factory(\App\User::class)->create();
+    // $address=new \App\Address([
+    //     'country'=>'indonesia'
+    // ]);
+    // $address->user()->associate($user);
+    // $address->save();
+    // $user->address()->create([
+    //     'country'=>'uk'
+    // ]);
+    $addresses=\App\Address::with('user')->get();
+    return view('users.index',compact('addresses'));
+});
